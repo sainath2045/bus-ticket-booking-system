945503efb130653bcd27c458ec9ca5f6c967321c
sainath2045 <151537734+sainath2045@users.noreply.github.com>
2023-12-05 23:12:02 +0530

    Update database.php

--- inc/database.php
+++ inc/database.php
@@ -1,7 +1,7 @@
 <?php
 function initDB()
 {
-    $connStr = mysqli_connect("localhost","root","","bus_ticket");
+    $connStr = mysqli_connect("sainath.mysql.database.azure.com","sainath","","bus_ticket");
     if ($connStr->connect_error)
         die("<script>alert('Connection failed: " . $connStr->connect_error . "');</script>");
     return $connStr;
